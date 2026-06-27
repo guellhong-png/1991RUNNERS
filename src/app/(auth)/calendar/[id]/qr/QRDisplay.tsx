eb8a9bfd80cf3e3be0524981d97d2dd38062d216
guellhong-png <guell.hong@bunjang.co.kr>
2026-06-27 09:52:47 +0900

    Update QRDisplay.tsx

--- src/app/(auth)/calendar/[id]/qr/QRDisplay.tsx
+++ src/app/(auth)/calendar/[id]/qr/QRDisplay.tsx
@@ -1,1 +1,26 @@
+'use client'
+import { useEffect, useRef } from 'react'
 
+export default function QRDisplay({ url }: { url: string }) {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const loadQR = async () => {
+      const QRCode = (await import('qrcode')).default
+      if (canvasRef.current) {
+        QRCode.toCanvas(canvasRef.current, url, {
+          width: 280,
+          margin: 2,
+          color: { dark: '#000000', light: '#ffffff' },
+        })
+      }
+    }
+    loadQR()
+  }, [url])
+
+  return (
+    <div className="bg-white p-6 rounded-2xl shadow-2xl">
+      <canvas ref={canvasRef} />
+    </div>
+  )
+}
