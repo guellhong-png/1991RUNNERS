4786b6c5eeee8bcf2c4d05a9d61414aa30d7f342
guellhong-png <guell.hong@bunjang.co.kr>
2026-07-02 12:23:29 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,27 @@
-self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', () => self.clients.claim())
+const CACHE_NAME = 'ddikkkoyang-v1'
+const PRECACHE_URLS = ['/dashboard', '/manifest.json', '/icon-512.png']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request))
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  )
 })
